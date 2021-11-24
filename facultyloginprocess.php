1514bdb007163e7bb20a0638baa3b4477ac642fb
matthewbondoc <89152311+matthewbondoc@users.noreply.github.com>
2021-11-24 20:12:17 +0800

    Back end continuation

--- facultyloginprocess.php
+++ facultyloginprocess.php
@@ -1,1 +1,25 @@
-/
+<?php
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    //Database connection here
+    $con = new mysqli('localhost', 'root', '', 'eu_database');
+    if($con->connect_error) {
+        die("Failed to connect: ".$con->connect_error);
+    } else {
+        $stmt = $con->prepare("SELECT * FROM `faculty` WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt_result = $stmt->get_result();
+        if($stmt_result->num_rows > 0) {
+            $data = $stmt_result->fetch_assoc();
+            if($data['password'] === $password) {
+                header("location: facultysuccess.php");
+            } else {
+                header("location: facultyfailed.php");
+            }
+        } else {
+            header("location: facultyfailed.php");
+        }
+    }
+?>
