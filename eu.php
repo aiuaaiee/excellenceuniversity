05352f99c1bca806fd86afbf45d22d401226fde1
JanKerwin <90701082+JanKerwin@users.noreply.github.com>
2021-11-24 20:08:07 +0800

    Front-end continuation

--- eu.php
+++ eu.php
@@ -1,1 +1,59 @@
-/
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>Excellence University</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="icon"
+        href="https://www.pngitem.com/pimgs/m/621-6218014_huddle-icons-students-per-school-school-icon-png.png"
+        type="icon type">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <style>
+        body {
+            font-family: 'Times New Roman', Times, serif;
+        }
+
+        .center {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 35%;
+        }
+
+        .center-1 {
+            background-color: #0099ff;
+            color: white;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 20%;
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <img src="EU LOGO.png" class="center">
+
+    <form method="POST" action="studentlogin.php">
+        <button type="submit" class="center-1">Student Portal</button>
+      </form>
+
+      <br>
+
+    <form method="POST" action="facultylogin.php">
+        <button type="submit" class="center-1">Faculty Portal</button>
+      </form>
+
+      <br>
+
+    <form method="POST" action="dashstudent.php">
+        <button type="submit" class="center-1">Admin Dashboard</button>
+      </form>
+
+</body>
+
+</html>
