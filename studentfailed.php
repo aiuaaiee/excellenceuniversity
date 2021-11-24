9b1ba4750509be10a0fbc40998897094b9541672
Ailene Advincula <90735622+aiuaaiee@users.noreply.github.com>
2021-11-24 20:16:21 +0800

    front end continuation

--- studentfailed.php
+++ studentfailed.php
@@ -1,1 +1,30 @@
-/
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Excellence University</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <link rel="icon"
+            href="https://www.pngitem.com/pimgs/m/621-6218014_huddle-icons-students-per-school-school-icon-png.png"
+            type="icon type">
+
+        <style>
+            body {
+                font-family: 'Times New Roman', Times, serif;
+            }
+
+            .center {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 35%;
+            }
+        </style>
+    </head>
+
+    <body>
+
+        <a href="eu.php"><img src="EU LOGO.png" class="center"></a>
+        <h2 style="text-align: center;">Invalid student email or password. <a href="studentlogin.php">Click here</a> to login again.</h2>
+        
+    </body>
+</html>
